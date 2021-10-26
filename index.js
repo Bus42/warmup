29c5498a852897026f3ccd0dc12152c326d2dc9f
Greg <gbrewton1980@gmail.com>
2021-10-26 17:55:56 -0600

    Creates Hello World server

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log("hello Node");
+
+const http = require("http");
+
+const hostname = "127.0.0.1";
+const port = 5000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello Node");
+});
+
+server.listen(port, hostname, () => {
+  console.log(`server listening on http://${hostname}:${port}`);
+});
