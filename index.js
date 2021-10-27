c1967a36c362946e4533ca82a250f30a5d2d641d
Greg <gbrewton1980@gmail.com>
2021-10-26 18:32:18 -0600

    changes port on index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ console.log("hello Node");
 const http = require("http");
 
 const hostname = "127.0.0.1";
-const port = 5000;
+const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
