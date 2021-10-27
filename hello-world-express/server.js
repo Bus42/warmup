7bdbd0a6b2d01a738be840ea204da2d1677aaa2c
Greg <gbrewton1980@gmail.com>
2021-10-26 19:16:38 -0600

    Adds route

--- hello-world-express/server.js
+++ hello-world-express/server.js
@@ -1,17 +1,30 @@
 const express = require("express");
-
 const hostname = "127.0.0.1";
 const port = 5000;
-
 const server = express();
+
+const hobbits = [
+  {
+    id: 1,
+    name: 'Samwise Gamgee',
+  },
+  {
+    id: 2,
+    name: 'Frodo Baggins',
+  },
+];
+
+server.get("/foo", (req, res) => {
+  res.send("bar")
+})
+
+server.get("/hobbits", (req, res) => {
+  res.status(200).json(hobbits);
+})
 
 server.get("/", (req, res) => {
   res.send("Hello Express!");
 });
-
-server.get("/foo", (req, res) => {
-    res.send("bar")
-})
 
 server.listen(port, hostname, () => {
   console.log(`server listening on http://${hostname}:${port}`);
