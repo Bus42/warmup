6410f30848c851617542dc3cda51ccc634cf8c85
Greg <gbrewton1980@gmail.com>
2021-10-28 19:04:50 -0600

    WIP: testing POST to /hobbits

--- hello-world-express/server.js
+++ hello-world-express/server.js
@@ -1,7 +1,9 @@
 const express = require("express");
+
 const hostname = "127.0.0.1";
 const port = 5000;
 const server = express();
+server.use(express.json)
 
 const hobbits = [
   {
@@ -18,13 +20,20 @@ server.get("/foo", (req, res) => {
   res.send("bar")
 })
 
+server.get("/", (req, res) => {
+  res.send("Hello Express!");
+});
+
 server.get("/hobbits", (req, res) => {
   res.status(200).json(hobbits);
 })
 
-server.get("/", (req, res) => {
-  res.send("Hello Express!");
-});
+server.post("/hobbits", (req, res) => {
+  // const hobbit = {...req.body, id: Date.now()};
+  // hobbits.push(hobbit);
+  // res.status(201).send(hobbits)
+  res.send("record created");
+})
 
 server.listen(port, hostname, () => {
   console.log(`server listening on http://${hostname}:${port}`);
